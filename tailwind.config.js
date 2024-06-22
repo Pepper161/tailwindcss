0b7b0321860ff7b13ca017ecee675575a634e933
pepper161 <ichikawaforbusiness@gmail.com>
2024-06-22 14:42:30 +0900

    first meessage

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    backgroundImage: {
+      "turorial-bg": "url('../src/img/main-bg.jpg')",
+    }
+  },
+  plugins: [],
+}
+
